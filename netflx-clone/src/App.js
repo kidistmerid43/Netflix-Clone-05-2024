0134d90d4e51bf1c96d16ee5f99361e83f8ce69e
kidistmerid43 <kidistmerid43@gmail.com>
2024-05-11 07:18:53 -0700

    header and footer

--- netflx-clone/src/App.js
+++ netflx-clone/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Home from "../src/Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <Home/>
     </div>
   );
 }
